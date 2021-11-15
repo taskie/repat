96dc81baa7a8b4f45eae82b1cd9cb9532d743301
taskie <t@skie.jp>
2021-11-15 23:25:59 +0900

    feat: replace string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::path::PathBuf;
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    path::PathBuf,
+};
 
 use anyhow::Result;
 use log::debug;
@@ -23,5 +27,14 @@ fn main() -> Result<()> {
     env_logger::init();
     let opt = Opt::from_args();
     debug!("{:?}", opt);
+    for file_path in opt.files.iter() {
+        let file = File::open(file_path)?;
+        let mut bufr = BufReader::new(file);
+        let mut bs = Vec::<u8>::new();
+        bufr.read_to_end(&mut bs)?;
+        let haystack = String::from_utf8(bs)?;
+        let replaced = opt.pattern.replace_all(&haystack, &opt.replacer);
+        print!("{}", replaced);
+    }
     Ok(())
 }
