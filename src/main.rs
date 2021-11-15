35921c64a6c7c5764c1fcfaae33e1044e3a62313
taskie <t@skie.jp>
2021-11-16 00:05:18 +0900

    feat: output patch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader},
-    path::PathBuf,
+    io::{self, BufRead, BufReader, BufWriter, Write},
+    path::{Path, PathBuf},
 };
 
 use anyhow::Result;
 use log::debug;
 use regex::Regex;
+use similar::TextDiff;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -23,12 +24,16 @@ struct Opt {
     files: Vec<PathBuf>,
 }
 
-fn replace<R: BufRead>(opt: &Opt, mut bufr: R) -> Result<()> {
+fn replace<R: BufRead, W: Write>(opt: &Opt, mut bufr: R, w: W, file_path: &Path) -> Result<()> {
     let mut bs = Vec::<u8>::new();
     bufr.read_to_end(&mut bs)?;
     let haystack = String::from_utf8(bs)?;
     let replaced = opt.pattern.replace_all(&haystack, &opt.replacer);
-    print!("{}", replaced);
+    let diff = TextDiff::from_lines(haystack.as_str(), &replaced);
+    let path_str = file_path.to_string_lossy();
+    diff.unified_diff()
+        .header(&path_str, &path_str)
+        .to_writer(w)?;
     Ok(())
 }
 
@@ -41,16 +46,19 @@ fn main() -> Result<()> {
     } else {
         opt.files.clone()
     };
+    let stdout = io::stdout();
+    let stdout_lock = stdout.lock();
+    let mut bufw = BufWriter::new(stdout_lock);
     for file_path in files.iter() {
         if file_path.to_str() == Some("-") {
             let stdin = io::stdin();
             let stdin_lock = stdin.lock();
             let bufr = BufReader::new(stdin_lock);
-            replace(&opt, bufr)?;
+            replace(&opt, bufr, &mut bufw, file_path)?;
         } else {
             let file = File::open(file_path)?;
             let bufr = BufReader::new(file);
-            replace(&opt, bufr)?;
+            replace(&opt, bufr, &mut bufw, file_path)?;
         }
     }
     Ok(())
