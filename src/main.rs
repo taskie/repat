cc15be651a74e96b46e027d27081cbc325c8f44a
taskie <t@skie.jp>
2021-11-15 23:38:48 +0900

    feat: support stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufReader, Read},
+    io::{self, BufRead, BufReader},
     path::PathBuf,
 };
 
@@ -23,18 +23,35 @@ struct Opt {
     files: Vec<PathBuf>,
 }
 
+fn replace<R: BufRead>(opt: &Opt, mut bufr: R) -> Result<()> {
+    let mut bs = Vec::<u8>::new();
+    bufr.read_to_end(&mut bs)?;
+    let haystack = String::from_utf8(bs)?;
+    let replaced = opt.pattern.replace_all(&haystack, &opt.replacer);
+    print!("{}", replaced);
+    Ok(())
+}
+
 fn main() -> Result<()> {
     env_logger::init();
     let opt = Opt::from_args();
     debug!("{:?}", opt);
-    for file_path in opt.files.iter() {
-        let file = File::open(file_path)?;
-        let mut bufr = BufReader::new(file);
-        let mut bs = Vec::<u8>::new();
-        bufr.read_to_end(&mut bs)?;
-        let haystack = String::from_utf8(bs)?;
-        let replaced = opt.pattern.replace_all(&haystack, &opt.replacer);
-        print!("{}", replaced);
+    let files = if opt.files.is_empty() {
+        vec![PathBuf::from("-")]
+    } else {
+        opt.files.clone()
+    };
+    for file_path in files.iter() {
+        if file_path.to_str() == Some("-") {
+            let stdin = io::stdin();
+            let stdin_lock = stdin.lock();
+            let bufr = BufReader::new(stdin_lock);
+            replace(&opt, bufr)?;
+        } else {
+            let file = File::open(file_path)?;
+            let bufr = BufReader::new(file);
+            replace(&opt, bufr)?;
+        }
     }
     Ok(())
 }
